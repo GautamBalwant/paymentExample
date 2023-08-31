5d70b0e23a4585d87fd636e22ddafe451a38b82d
GautamBalwant <100532564+GautamBalwant@users.noreply.github.com>
2023-08-31 16:43:02 +0530

    initial Commit

---
+++ src/main/java/com/payment/simulator/SimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.payment.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatorApplication.class, args);
+	}
+
+}
